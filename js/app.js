cafb3b35507701635db8ad08d288ae517ff31378
Thomas-Bezille <bezille.thomasb@gmail.com>
2023-12-10 13:45:38 +0100

    Fermeture des autres réponse lors de l'ouverture d'une

--- js/app.js
+++ js/app.js
@@ -3,13 +3,32 @@ const app = {
     const questions = document.querySelectorAll(".question-container");
     for (const question of questions) {
       question.addEventListener("click", (event) => {
+        app.hideAnswerElem();
         app.handleClickQuestion(event);
       });
     }
   },
   handleClickQuestion: function (event) {
     const response = event.currentTarget.querySelector(".answer");
+    const iconPlus = event.currentTarget.querySelector(".plus");
+    const iconMinus = event.currentTarget.querySelector(".minus");
     response.classList.toggle("hidden");
+    iconPlus.classList.toggle("hidden");
+    iconMinus.classList.toggle("hidden");
+  },
+  hideAnswerElem: function () {
+    const answers = document.querySelectorAll(".answer");
+    const iconsPlus = document.querySelectorAll(".plus");
+    const iconsMinus = document.querySelectorAll(".minus");
+    for (const answer of answers) {
+      answer.classList.add("hidden");
+      for (const iconPlus of iconsPlus) {
+        iconPlus.classList.remove("hidden");
+        for (const iconMinus of iconsMinus) {
+          iconMinus.classList.add("hidden");
+        }
+      }
+    }
   },
 };
 
